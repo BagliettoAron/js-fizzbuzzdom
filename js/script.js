0443581a5c6cb457c7b6ff0b3da742f59397daef
@BagliettoAron <aronbaglietto88@gmail.com>
2022-04-11 14:52:59 +0200

    Add: script in js for determinate if a numer is Fizz, Buzz or FizzBuzz

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-alert("ciao")
+// Scrivi un programma che stampi in console i numeri da 1 a 100.
+// Per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+
+// 1. stampa nella console
+for (let i = 1; i <= 100; i++) {
+
+    // 1.a. definire se sono multipli di 3
+    let multipleOf = "";
+
+    if (i % 3 === 0) {
+        multipleOf = "Fizz";
+        if (i % 5 === 0){
+            multipleOf = "FizzBuzz";
+        }
+
+    }else if (i % 5 === 0) { 
+        multipleOf = "Buzz";
+        if (i % 3 === 0){
+            multipleOf = "FizzBuzz";
+        }
+    }
+
+    console.log(i, multipleOf)
+}
